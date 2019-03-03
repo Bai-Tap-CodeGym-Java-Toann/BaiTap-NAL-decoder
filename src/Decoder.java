afef199ffd6917bdd84a3df6a443113f5fb1577e
lana <sitdownrightnow2552@gmail.com>
2019-03-03 10:11:38 +0700

    method to check decode

--- src/Decoder.java
+++ src/Decoder.java
@@ -16,20 +16,20 @@ public class Decoder {
         return deShifted;
     }
 
-    public String getAddedKey(String added) throws Exception {
-        if (added.length() % 2 != 0)
-            throw new Exception("This not an Added String");
-
-        String[] addedChars = added.split("");
-        String[] addedKeyChars = new String[addedChars.length / 2];
-        for (int i = 0; i < addedChars.length; i++) {
-            if (i % 2 != 0) {
-                int addedKeyIndex = (int) Math.ceil((float)(i / 2));
-                addedKeyChars[addedKeyIndex] = addedChars[i];
-            }
-        }
-        return String.join("", addedKeyChars);
-    }
+//    public String getAddedKey(String added) throws Exception {
+//        if (added.length() % 2 != 0)
+//            throw new Exception("This not an Added String");
+//
+//        String[] addedChars = added.split("");
+//        String[] addedKeyChars = new String[addedChars.length / 2];
+//        for (int i = 0; i < addedChars.length; i++) {
+//            if (i % 2 != 0) {
+//                int addedKeyIndex = (int) Math.ceil((float) (i / 2));
+//                addedKeyChars[addedKeyIndex] = addedChars[i];
+//            }
+//        }
+//        return String.join("", addedKeyChars);
+//    }
 
     private String deAdd(String added) throws Exception {
         if (added.length() % 2 != 0)
@@ -48,5 +48,18 @@ public class Decoder {
     private String deShift(String shifted, int keyLength) {
         int shiftLength = keyLength - 1;
         return (shifted.substring(shiftLength) + shifted.substring(0, shiftLength));
+    }
+
+    private boolean isFullyDecoded(String decoded, String key) throws Exception {
+        String keyPart = decoded.substring(decoded.length() - key.length());
+        boolean isKeyPartMatch = keyPart.equals(key);
+        boolean isAbleToBeAnAddedString = decoded.length() / 2 == 0;
+        boolean isMinimumLengthSatisfied = decoded.length() >= key.length();
+        boolean isDecodeAble = isMinimumLengthSatisfied && isAbleToBeAnAddedString;
+        if (isKeyPartMatch)
+            return true;
+        if (!isDecodeAble)
+            throw new Exception("my decoder can not cover this case");
+        return false;
     }
 }
