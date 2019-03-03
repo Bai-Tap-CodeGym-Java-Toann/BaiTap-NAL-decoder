cb4e514b4c7789c8bb535232b8e26f9d7d71fb59
lana <sitdownrightnow2552@gmail.com>
2019-03-03 11:54:57 +0700

    finished.

--- src/Decoder.java
+++ src/Decoder.java
@@ -1,53 +1,37 @@
 public class Decoder {
     public String decode(String input, String key) throws Exception {
         while (!isFullyDecoded(input, key)) {
-            input = decodeOneTime(input, key.length());
+            input = decodeOneTime(input, key);
         }
         return input.substring(0, input.length() - key.length());
     }
 
-    /*
-     * This method is very low-accuracy as is not tested and
-     * can easily by pass :(
-     */
     private boolean isFullyDecoded(String decoded, String key) throws Exception {
-        String keyPart = decoded.substring(decoded.length() - key.length());
-        boolean isKeyPartMatch = keyPart.equals(key);
-        boolean isAbleToBeAnAddedString = decoded.length() % 2 == 0;
-        boolean isMinimumLengthSatisfied = decoded.length() >= key.length();
-        boolean isDecodeAble = isMinimumLengthSatisfied && isAbleToBeAnAddedString;
-        if (isKeyPartMatch)
-            return true;
-        if (!isDecodeAble)
-            throw new Exception("my decoder can not cover this case");
-        return false;
+        return decoded.endsWith(key);
     }
 
-    private String decodeOneTime(String input, int keyLength) {
+    private String decodeOneTime(String input, String key) throws Exception {
         String deAdded, deShifted;
-        try {
-            deAdded = deAdd(input);
-        } catch (Exception e) {
-            deAdded = input;
-        }
-        deShifted = deShift(deAdded, keyLength);
+        deAdded = deAdd(input, key);
+        deShifted = deShift(deAdded, key);
         return deShifted;
     }
 
-    private String deAdd(String added) throws Exception {
-        if (added.length() % 2 != 0)
-            throw new Exception("This not an Added String");
-        char[] deAddedChars = new char[added.length() / 2];
-        for (int i = 0; i < added.length(); i++) {
-            if (i % 2 == 0) {
-                deAddedChars[i / 2] = added.charAt(i);
-            }
+    private String deAdd(String added, String key) throws Exception {
+        char[] deAddedChars = new char[added.length()];
+        for (int addedIndex = 0; addedIndex < added.length(); addedIndex++) {
+            int keyIndex = addedIndex % key.length();
+            int addedCharCode = (int) added.charAt(addedIndex);
+            int keyCharCode = (int) key.charAt(keyIndex);
+            if (keyCharCode >= addedCharCode)
+                throw new Exception("key not valid");
+            deAddedChars[addedIndex] = (char) (addedCharCode - keyCharCode);
         }
         return String.copyValueOf(deAddedChars);
     }
 
-    private String deShift(String shifted, int keyLength) {
-        int shiftLength = keyLength - 1;
+    private String deShift(String shifted, String key) {
+        int shiftLength = key.length() - 1;
         return (shifted.substring(shiftLength) + shifted.substring(0, shiftLength));
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,10 @@ public class Main {
     public static void main(String[] args) throws Exception {
         Decoder decoder = new Decoder();
         String encoded = "тҢ⊞ЭЏ҅⌦јϘњҊԠѥРЏҊԟѥПЏԳ⌂ЗХѐҐҦЗФ⊐ҋџўСӐтҳѩ⊃ЏҖ⌐ѫϘѝҊ⋤ѫϘђҊҮЗУїԃҢџϘїԂҭўϦњ҆ѢМВжѕԛџѣҘќбҘҨ⊾ЛЏѸԎѫЗФѤԅҐџљ≧ѣбԳ⋦ѬϘѣ⋼тҵѠ≿ђбҖԫѤϘїѺ⋥ҴЗУїӲ҅ҧЗРӏѿ҉ѫЗМ⊺ѴҊџѭ⊝Џ҇ԂџѥРѤ";
-        String key = "F{D\\\"||_ }";
+        String key = "kd#%ZG";
+        String notValidKey = "fadia8#FADG";
         System.out.println(decoder.decode(encoded, key));
+        System.out.println(decoder.decode(encoded, notValidKey));
 
     }
 
